e4f1691861774d71b274c253394d61ed5a55c7cc
VictorNoe <20213tn059@utez.edu.mx>
2024-02-28 23:03:38 -0600

    first commit

---
+++ segurity_backend/src/main/java/mx/edu/utez/segurity_backend/SegurityBackendApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.segurity_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegurityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegurityBackendApplication.class, args);
+	}
+
+}
